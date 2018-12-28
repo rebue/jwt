e0c4dd295a0d07edc6aeb48d37f0b3b71aae2079
zbz <nnzbz@qq.com>
2018-12-28 22:12:20 +0800

    * 修正字典类

--- jwt-api/src/main/java/rebue/scx/jwt/dic/JwtSignResultDic.java
+++ jwt-api/src/main/java/rebue/scx/jwt/dic/JwtSignResultDic.java
@@ -33,7 +33,7 @@ public enum JwtSignResultDic implements EnumBase {
     // 初始化map，保存枚举的所有项到map中以方便通过code查找
     static {
         valueMap = new HashMap<>();
-        for (EnumBase item : values()) {
+        for (final EnumBase item : values()) {
             valueMap.put(item.getCode(), item);
         }
     }
@@ -42,8 +42,8 @@ public enum JwtSignResultDic implements EnumBase {
      * jackson反序列化时，通过code得到枚举的实例 注意：此方法必须是static的方法，且返回类型必须是本枚举类，而不能是接口EnumBase 否则jackson将调用默认的反序列化方法，而不会调用本方法
      */
     @JsonCreator
-    public static JwtSignResultDic getItem(int code) {
-        EnumBase result = valueMap.get(code);
+    public static JwtSignResultDic getItem(final int code) {
+        final EnumBase result = valueMap.get(code);
         if (result == null) {
             throw new IllegalArgumentException("输入的code" + code + "不在枚举的取值范围内");
         }
@@ -55,7 +55,7 @@ public enum JwtSignResultDic implements EnumBase {
     /**
      * 构造器，传入code
      */
-    JwtSignResultDic(int code) {
+    JwtSignResultDic(final int code) {
         this.code = code;
     }
 
@@ -65,6 +65,11 @@ public enum JwtSignResultDic implements EnumBase {
     @Override
     public int getCode() {
         return code;
+    }
+
+    @Override
+    public String getName() {
+        return name();
     }
 
 }
